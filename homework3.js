ff47609a3ce15aa0b3f0e7b581caae4f55df0559
anuragpatsariya <anurag.patsariya@outlook.com>
2016-03-07 23:38:54 -0800

    strict changes

--- homework3.js
+++ homework3.js
@@ -1,8 +1,8 @@
-"use strict";
 /* jshint browser:true */
 /* globals _ */
 /*----------------------------( Exercise 1 )---------------------------------------------------*/
-var excercise1 = function (nums) {
+var excercise1 = function(nums) {
+    "use strict";
     var sum = 0,
         avg = 0,
         len = nums.length,
@@ -14,12 +14,14 @@ var excercise1 = function (nums) {
     return avg;
 };
 
-(function () {
+(function() {
+    "use strict";
     document.getElementById("exercise1").innerHTML += "<strong><em>Exercise 1</em></strong><br> The average of the array=[2, 3, 4, 5] is: <strong>" + excercise1([2, 3, 4, 5]).toString() + "</strong>";
 })();
 
 /*----------------------------( Exercise 2 )---------------------------------------------------*/
-var excercise2 = function (nums) {
+var excercise2 = function(nums) {
+    "use strict";
     var largest = -Infinity,
         len = nums.length,
         i;
@@ -32,21 +34,25 @@ var excercise2 = function (nums) {
     return largest;
 };
 
-(function () {
+(function() {
+    "use strict";
     document.getElementById("exercise2").innerHTML += "<strong><em>Exercise 2</em></strong><br> The maximum element in array=[3, 4, 6, 98, 12, 6] is: <strong>" + excercise2([3, 4, 6, 98, 12, 6]).toString() + "</strong>";
 })();
 
 //using underscore.js
-var us_excercise2 = function (nums) {
+var us_excercise2 = function(nums) {
+    "use strict";
     return _.max(nums);
 };
 
-(function () {
+(function() {
+    "use strict";
     document.getElementById("exercise2").innerHTML += " </br><em> Using underscore.js: </em><strong>" + us_excercise2([3, 4, 6, 98, 12, 6]).toString() + "</strong>";
 })();
 
 /*-----------------------------( Exercise 3 )-----------------------------------------------------*/
-var excercise3 = function (nums) {
+var excercise3 = function(nums) {
+    "use strict";
     var i,
         len = nums.length;
     for (i = 0; i < len; i++) {
@@ -57,21 +63,25 @@ var excercise3 = function (nums) {
     }
     return false;
 };
-(function () {
+(function() {
+    "use strict";
     document.getElementById("exercise3").innerHTML += "<strong><em>Exercise 3</em></strong><br> The array=[5, 77, 99, 87, 66, 57, 23] contains atleast a even number is: <strong>" + excercise3([5, 77, 99, 87, 66, 57, 23]).toString() + "</strong>";
 })();
 
 //using underscore.js
-var us_excercise3 = function (nums) {
-    return _.some(nums, function (n) {
+var us_excercise3 = function(nums) {
+    "use strict";
+    return _.some(nums, function(n) {
         return n % 2 === 0;
     });
 };
-(function () {
+(function() {
+    "use strict";
     document.getElementById("exercise3").innerHTML += "</br><em> Using underscore.js: </em><strong>" + us_excercise3([5, 77, 99, 87, 66, 57, 23]).toString() + "</strong>";
 })();
 /*--------------------------------( Exercise 4 )---------------------------------------------------*/
-var excercise4 = function (nums) {
+var excercise4 = function(nums) {
+    "use strict";
     var i,
         len = nums.length;
     for (i = 0; i < len; i++) {
@@ -82,21 +92,25 @@ var excercise4 = function (nums) {
     }
     return true;
 };
-(function () {
+(function() {
+    "use strict";
     document.getElementById("exercise4").innerHTML += "<strong><em>Exercise 4</em></strong><br> The array=[52, 772, 992, 872, 66, 5712, 24] contains all even number is: <strong>" + excercise4([52, 772, 992, 872, 66, 5712, 24]).toString() + "</strong>";
 })();
 
 //using underscore.js
-var us_excercise4 = function (nums) {
-    return _.every(nums, function (n) {
+var us_excercise4 = function(nums) {
+    "use strict";
+    return _.every(nums, function(n) {
         return n % 2 === 0;
     });
 };
-(function () {
+(function() {
+    "use strict";
     document.getElementById("exercise4").innerHTML += "</br><em> Using underscore.js: </em><strong>" + us_excercise4([52, 772, 992, 872, 66, 5712, 24]).toString() + "</strong>";
 })();
 /*----------------------------------( Exercise 5 )------------------------------------------------------*/
-var arrayContains = function (arr_str, str) {
+var arrayContains = function(arr_str, str) {
+    "use strict";
     var i;
     for (i = 0; i < arr_str.length; i++) {
         if (arr_str[i] === str) {
@@ -105,11 +119,13 @@ var arrayContains = function (arr_str, str) {
     }
     return false;
 };
-(function () {
+(function() {
+    "use strict";
     document.getElementById("exercise5").innerHTML += "<strong><em>Exercise 5</em></strong><br> The array=[\"hello\", \"world\", \"wide\", \"web\"] contains \"wide\" is: <strong>" + arrayContains(["hello", "world", "wide", "web"], "wide").toString() + "</strong>";
 })();
 /*---------------------------------( Exercise 6 )--------------------------------------------------------*/
-var arrayContainsTwo = function (arr_str, str) {
+var arrayContainsTwo = function(arr_str, str) {
+    "use strict";
     var i;
     var count = 0;
     for (i = 0; i < arr_str.length; i++) {
@@ -122,13 +138,15 @@ var arrayContainsTwo = function (arr_str, str) {
     }
     return false;
 };
-(function () {
+(function() {
+    "use strict";
     document.getElementById("exercise6").innerHTML += "<strong><em>Exercise 6<br>Part 1: </em></strong> The array=[\"hello\", \"abc\", \"web\", \"web\"] contains \"web\" atleast two times is: <strong>" + arrayContainsTwo(["hello", "abc", "web", "web"], "web").toString() + "</strong>";
 })();
 
 
 //Now for three count
-var arrayContainsThree = function (arr_str, str) {
+var arrayContainsThree = function(arr_str, str) {
+    "use strict";
     var i;
     var count = 0;
     for (i = 0; i < arr_str.length; i++) {
@@ -141,12 +159,14 @@ var arrayContainsThree = function (arr_str, str) {
     }
     return false;
 };
-(function () {
+(function() {
+    "use strict";
     document.getElementById("exercise6").innerHTML += "<br><strong><em>Part 2: </em></strong> The array=[\"hello\", \"web\", \"web\", \"web\"] contains \"web\" atleast three times is: <strong>" + arrayContainsThree(["hello", "web", "web", "web"], "web").toString() + "</strong>";
 })();
 
 //Now for N  count
-var arrayContainsNTimes = function (arr_str, str, n) {
+var arrayContainsNTimes = function(arr_str, str, n) {
+    "use strict";
     var i;
     var count = 0;
     for (i = 0; i < arr_str.length; i++) {
@@ -160,7 +180,7 @@ var arrayContainsNTimes = function (arr_str, str, n) {
     return false;
 
 };
-(function () {
+(function() {
+    "use strict";
     document.getElementById("exercise6").innerHTML += "<br><strong><em>Part 3: </em></strong> The array=[\"hello\", \"web\", \"web\", \"web\"] contains \"web\" n=4 times is: <strong>" + arrayContainsNTimes(["world", "web", "web", "web"], "web", 4).toString() + "</strong>";
 })();
-
